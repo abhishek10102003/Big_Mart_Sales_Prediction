794b01c34ef1dddaf5c0a47d06215ede6070609d
abhishek10102003 <84247723+abhishek10102003@users.noreply.github.com>
2022-07-04 13:58:25 +0530

    Add files via upload

---
+++ Big_Mart_Sales_Prediction.py
@@ -0,0 +1,375 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Importing the Dependencies
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn import metrics
+
+
+# Data Collection and Processing
+
+# In[2]:
+
+
+# loading the data from csv file to Pandas DataFrame
+big_mart_data = pd.read_csv('C:/Users/abhis/Downloads/Train.csv')
+
+
+# In[3]:
+
+
+# first 5 rows of the dataframe
+big_mart_data.head()
+
+
+# In[4]:
+
+
+# number of data points & number of features
+big_mart_data.shape
+
+
+# In[5]:
+
+
+# getting some information about thye dataset
+big_mart_data.info()
+
+
+# Categorical Features:
+# - Item_Identifier
+# - Item_Fat_Content
+# - Item_Type
+# - Outlet_Identifier
+# - Outlet_Size
+# - Outlet_Location_Type
+# - Outlet_Type
+
+# In[6]:
+
+
+# checking for missing values
+big_mart_data.isnull().sum()
+
+
+# Handling Missing Values
+
+# Mean --> average
+# 
+# Mode --> more repeated value
+
+# In[7]:
+
+
+# mean value of "Item_Weight" column
+big_mart_data['Item_Weight'].mean()
+
+
+# In[8]:
+
+
+# filling the missing values in "Item_weight column" with "Mean" value
+big_mart_data['Item_Weight'].fillna(big_mart_data['Item_Weight'].mean(), inplace=True)
+
+
+# In[9]:
+
+
+# mode of "Outlet_Size" column
+big_mart_data['Outlet_Size'].mode()
+
+
+# In[10]:
+
+
+# filling the missing values in "Outlet_Size" column with Mode
+mode_of_Outlet_size = big_mart_data.pivot_table(values='Outlet_Size', columns='Outlet_Type', aggfunc=(lambda x: x.mode()[0]))
+
+
+# In[11]:
+
+
+print(mode_of_Outlet_size)
+
+
+# In[12]:
+
+
+miss_values = big_mart_data['Outlet_Size'].isnull()   
+
+
+# In[13]:
+
+
+print(miss_values)
+
+
+# In[14]:
+
+
+big_mart_data.loc[miss_values, 'Outlet_Size'] = big_mart_data.loc[miss_values,'Outlet_Type'].apply(lambda x: mode_of_Outlet_size[x])
+
+
+# In[15]:
+
+
+# checking for missing values
+big_mart_data.isnull().sum()
+
+
+# Data Analysis
+
+# In[16]:
+
+
+big_mart_data.describe()
+
+
+# Numerical Features
+
+# In[17]:
+
+
+sns.set()
+
+
+# In[18]:
+
+
+# Item_Weight distribution
+plt.figure(figsize=(6,6))
+sns.distplot(big_mart_data['Item_Weight'])
+plt.show()
+
+
+# In[19]:
+
+
+# Item Visibility distribution
+plt.figure(figsize=(6,6))
+sns.distplot(big_mart_data['Item_Visibility'])
+plt.show()
+
+
+# In[20]:
+
+
+# Item MRP distribution
+plt.figure(figsize=(6,6))
+sns.distplot(big_mart_data['Item_MRP'])
+plt.show()
+
+
+# In[21]:
+
+
+# Item_Outlet_Sales distribution
+plt.figure(figsize=(6,6))
+sns.distplot(big_mart_data['Item_Outlet_Sales'])
+plt.show()
+
+
+# In[22]:
+
+
+# Outlet_Establishment_Year column
+plt.figure(figsize=(6,6))
+sns.countplot(x='Outlet_Establishment_Year', data=big_mart_data)
+plt.show()
+
+
+# Categorical Features
+
+# In[23]:
+
+
+# Item_Fat_Content column
+plt.figure(figsize=(6,6))
+sns.countplot(x='Item_Fat_Content', data=big_mart_data)
+plt.show()
+
+
+# In[24]:
+
+
+# Item_Type column
+plt.figure(figsize=(30,6))
+sns.countplot(x='Item_Type', data=big_mart_data)
+plt.show()
+
+
+# In[25]:
+
+
+# Outlet_Size column
+plt.figure(figsize=(6,6))
+sns.countplot(x='Outlet_Size', data=big_mart_data)
+plt.show()
+
+
+# Data Pre-Processing
+
+# In[26]:
+
+
+big_mart_data.head()
+
+
+# In[27]:
+
+
+big_mart_data['Item_Fat_Content'].value_counts()
+
+
+# In[28]:
+
+
+big_mart_data.replace({'Item_Fat_Content': {'low fat':'Low Fat','LF':'Low Fat', 'reg':'Regular'}}, inplace=True)
+
+
+# In[29]:
+
+
+big_mart_data['Item_Fat_Content'].value_counts()
+
+
+# Label Encoding
+
+# In[30]:
+
+
+encoder = LabelEncoder()
+
+
+# In[31]:
+
+
+big_mart_data['Item_Identifier'] = encoder.fit_transform(big_mart_data['Item_Identifier'])
+
+big_mart_data['Item_Fat_Content'] = encoder.fit_transform(big_mart_data['Item_Fat_Content'])
+
+big_mart_data['Item_Type'] = encoder.fit_transform(big_mart_data['Item_Type'])
+
+big_mart_data['Outlet_Identifier'] = encoder.fit_transform(big_mart_data['Outlet_Identifier'])
+
+big_mart_data['Outlet_Size'] = encoder.fit_transform(big_mart_data['Outlet_Size'])
+
+big_mart_data['Outlet_Location_Type'] = encoder.fit_transform(big_mart_data['Outlet_Location_Type'])
+
+big_mart_data['Outlet_Type'] = encoder.fit_transform(big_mart_data['Outlet_Type'])
+
+
+# In[32]:
+
+
+big_mart_data.head()
+
+
+# Splitting features and Target
+
+# In[33]:
+
+
+X = big_mart_data.drop(columns='Item_Outlet_Sales', axis=1)
+Y = big_mart_data['Item_Outlet_Sales']
+
+
+# In[34]:
+
+
+print(X)
+
+
+# In[35]:
+
+
+print(Y)
+
+
+# Splitting the data into Training data & Testing Data
+
+# In[36]:
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+
+
+# In[37]:
+
+
+print(X.shape, X_train.shape, X_test.shape)
+
+
+# Machine Learning Model Training
+
+# XGBoost Regressor
+
+# In[38]:
+
+
+regressor = XGBRegressor()
+
+
+# In[39]:
+
+
+regressor.fit(X_train, Y_train)
+
+
+# Evaluation
+
+# In[40]:
+
+
+# prediction on training data
+training_data_prediction = regressor.predict(X_train)
+
+
+# In[41]:
+
+
+# R squared Value
+r2_train = metrics.r2_score(Y_train, training_data_prediction)
+
+
+# In[42]:
+
+
+print('R Squared value = ', r2_train)
+
+
+# In[43]:
+
+
+# prediction on test data
+test_data_prediction = regressor.predict(X_test)
+
+
+# In[44]:
+
+
+# R squared Value
+r2_test = metrics.r2_score(Y_test, test_data_prediction)
+
+
+# In[45]:
+
+
+print('R Squared value = ', r2_test)
+
+
+# In[ ]:
+
+
+
+
